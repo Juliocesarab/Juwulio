bd1810e92864ba1c175b969e44fe1755b6019004
Your Name <you@example.com>
2024-03-08 22:01:52 -0500

    mejora de card

--- Index.js
+++ Index.js
@@ -2,11 +2,38 @@ console.log("Hola mundo")
 
 const URL = "https://fakestoreapi.com/products";
 
-function getData(){
+let titulo = document.getElementById("titulo")
+let productos =document.getElementById("productos")
+let tabla = "<div class='card'>";
 
+
+
+function getData() {
     fetch(URL)
     .then( response => response.json() )
-    .then( data => console.log("Datos de la API", data) )
+    .then( data => {
+        console.log("Datos de la API", data) 
+        for(let i = 0; i<data.length; i++ ){
+            console.log(data[i].title)
+            let bloquehtml = 
+             `
+             <div class='card-item'>
+                <div class='cabecera'></div>
+                <img width="80px" src="${(data[i].image)}">
+                <p class='titulo'> ${(data[i].title)}</p>
+                <p>$ ${(data[i].price)}</p>
+                <p> ${(data[i].category)}</label>
+             </div>
+             `
+            
+            tabla += bloquehtml;
+        }
+        tabla += "<div>";
+        productos.innerHTML = tabla;
+    } )
+        
+    
 }
+
 
 getData();
