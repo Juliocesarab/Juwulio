73ab765f4dc6ce1c14cac475227b6a15f230e958
Your Name <you@example.com>
2024-03-15 21:58:37 -0500

    refactorizacion funcion reutilizable

--- Index.js
+++ Index.js
@@ -1,38 +1,73 @@
-console.log("Hola mundo")
-
 const URL = "https://fakestoreapi.com/products";
-
-let titulo = document.getElementById("titulo")
-let productos =document.getElementById("productos")
-let tabla = "<div class='card'>";
-
-
-
-function getData() {
+let productos = document.getElementById("productos")
+// let tabla = "<div class='card'>";
+function getData(){
     fetch(URL)
     .then( response => response.json() )
     .then( data => {
-        console.log("Datos de la API", data) 
-        for(let i = 0; i<data.length; i++ ){
-            console.log(data[i].title)
-            let bloquehtml = 
+        let productosCardSet = JSON.stringify(data)
+        localStorage.setItem('productos', productosCardSet )
+        mapearDatos(data);
+        mapearDatos(data);
+        // console.log("productosCardSet: ",productosCardSet)
+
+        // let productosCardGet = localStorage.getItem('productos')
+        // let listaProucto = JSON.parse(productosCardGet)
+        // console.log("listaProucto: ", listaProucto)
+
+        // for(let i = 0; i < listaProucto.length; i++ ){
+        //     let r = Math.floor( Math.random()*256 ) 
+        //     let g = Math.floor( Math.random()*256 ) 
+        //     let b = Math.floor( Math.random()*256 ) 
+        //     let precio = data[i].price
+        //      let bloqueHtml = 
+        //      `
+        //      <div class='card-item'>
+        //         <div class='cabecera' style="background: rgb(${r} ${g} ${b} );" ></div>
+        //         <div class='cont-img'>
+        //             <img src="${data[i].image}" />
+        //         </div>
+        //         <p class='titulo'>${data[i].title}</p>
+        //         <p>$${precio} <span class='precio-sd'> $${ ((precio * 0.1) + precio).toFixed(2)  }</span> </p>
+        //         <label class='categoria'>${data[i].category}</label>
+        //      </div>
+        //      `;
+        //      tabla += bloqueHtml;
+        // }
+        // tabla +="</div>";
+        // productos.innerHTML = tabla;
+    })
+}
+function mapearDatos(data){
+
+    let tabla = "<div class='card'>";
+    data.forEach(item => {
+        // console.log(item.title)
+        let r = Math.floor( Math.random()*256 ) 
+        let g = Math.floor( Math.random()*256 ) 
+        let b = Math.floor( Math.random()*256 ) 
+        let precio = item.price
+            let bloqueHtml = 
              `
              <div class='card-item'>
-                <div class='cabecera'></div>
-                <img width="80px" src="${(data[i].image)}">
-                <p class='titulo'> ${(data[i].title)}</p>
-                <p>$ ${(data[i].price)}</p>
-                <p> ${(data[i].category)}</label>
+                <div class='cabecera' style="background: rgb(${r} ${g} ${b} );" ></div>
+                <div class='cont-img'>
+                    <img src="${item.image}" />
+                </div>
+                <p class='titulo'>${item.title}</p>
+                <p>$${precio} <span class='precio-sd'> $${ ((precio * 0.1) + precio).toFixed(2)  }</span> </p>
+                <label class='categoria'>${item.category}</label>
              </div>
-             `
+             `;
+             tabla += bloqueHtml;
+    });
+
+    // for(let i = 0; i < listaProucto.length; i++ ){
             
-            tabla += bloquehtml;
-        }
-        tabla += "<div>";
-        productos.innerHTML = tabla;
-    } )
         
-    
+        tabla +="</div>";
+        productos.innerHTML = tabla;
+
 }
 
 
